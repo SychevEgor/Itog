72603ef09a0efd4a908cbfa8fbc4c42074482f25
Egor <tkuik255@gmail.com>
2022-11-09 13:26:24 +0300

    Этап 4

---
+++ prog/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] MyArray = new string[] {"hello", "2", "world",":-)"};
+string[] Myarray2 = new string[MyArray.Length];
+void ChekingCharacters(string[] MyArray, string[] MyArray2)
+{
+    int count = 0;
+    for (int i = 0; i < MyArray.Length; i++)
+    {
+    if(MyArray[i].Length <= 3)
+        {
+        MyArray2[count] = MyArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ChekingCharacters(MyArray, Myarray2);
+PrintArray(Myarray2);
